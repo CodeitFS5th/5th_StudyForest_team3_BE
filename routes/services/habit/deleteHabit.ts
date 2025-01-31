3d88f7ef990f702a8f198926e39ac893d1a961c2
HEES56 <rlagmltjd125@gmail.com>
2025-01-31 17:47:18 +0900

    add : habit(deleteHabit, updateHabit)

--- routes/services/habit/deleteHabit.ts
+++ routes/services/habit/deleteHabit.ts
@@ -1,6 +1,41 @@
 import prisma from "../../prisma";
 import { RequestHandler } from "express";
 
-const deleteHabit: RequestHandler = async (req, res) => {};
+const deleteHabit: RequestHandler = async (req, res, next) => {
+  try {
+    const habitId: number = Number(req.params.id);
+
+    // 삭제할 습관 조회
+    const habit = await prisma.habit.findUnique({
+      where: {
+        id: habitId,
+      },
+    });
+
+    if (!habit) {
+      res.status(404).send("삭제할 습관이 존재하지 않습니다!");
+      return;
+    }
+
+    // DeletedHabit 데이블 저장
+    await prisma.deletedHabit.create({
+      data: {
+        habit_id: habit.id,
+        name: habit.name,
+        status: habit.status,
+        study_id: habit.study_id,
+      },
+    });
+
+    // 원본 습관 테이블에서 삭제
+    await prisma.habit.delete({
+      where: { id: habitId },
+    });
+
+    res.status(204).send("요청하신 습관이 삭제 되었습니다!");
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default deleteHabit;
--- routes/services/habit/updateHabit.ts
+++ routes/services/habit/updateHabit.ts
@@ -1,6 +1,38 @@
 import prisma from "../../prisma";
 import { RequestHandler } from "express";
 
-const updateHabit: RequestHandler = async (req, res) => {};
+const updateHabit: RequestHandler = async (req, res, next) => {
+  try {
+    const habitId: number = Number(req.params.id);
+    const upData = req.body;
+
+    // 습관 삭제여부 확인
+    const deletedHabit = await prisma.deletedHabit.findUnique({
+      where: {
+        id: habitId,
+      },
+    });
+
+    if (deletedHabit) {
+      res.status(403).send("이미 삭제된 습관입니다!");
+      return;
+    }
+
+    // 습관 업데이트
+    const updatedHabit = await prisma.habit.update({
+      where: { id: habitId },
+      data: upData,
+    });
+
+    if (!updatedHabit) {
+      res.status(404).send("수정할 습관이 존재하지 않습니다!");
+      return;
+    }
+
+    res.status(200).send(updatedHabit);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default updateHabit;
