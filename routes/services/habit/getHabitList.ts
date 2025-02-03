ee594aba70c48f57606590239d5caa040c473114
김민희 <smnhree@gmail.com>
2025-02-03 14:35:13 +0900

    🎉 init (deleteStudy, getHabitList 구현, study 스키마 수정)

--- routes/services/habit/getHabitList.ts
+++ routes/services/habit/getHabitList.ts
@@ -1,6 +1,32 @@
 import prisma from "../../prisma";
 import { RequestHandler } from "express";
 
-const getHabitList: RequestHandler = async (req, res) => {};
+interface CustomError extends Error {
+  status?: number;
+}
+
+const getHabitList: RequestHandler = async (req, res, next) => {
+  try {
+    const { studyId }: { studyId: number } = {
+      studyId: Number(req.body.studyId),
+    };
+
+    const habitList = await prisma.habit.findMany({
+      where: {
+        study_id: studyId,
+      },
+    });
+
+    if (habitList.length === 0) {
+      const error: CustomError = new Error("습관이 존재하지 않습니다!");
+      error.status = 404;
+      throw error;
+    }
+
+    res.status(200).send({ habitList });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default getHabitList;
--- routes/services/study/deleteStudy.ts
+++ routes/services/study/deleteStudy.ts
@@ -1,8 +1,49 @@
 import prisma from "../../prisma";
 import { RequestHandler } from "express";
 
+interface CustomError extends Error {
+  status?: number;
+}
+
 const deleteStudy: RequestHandler = async (req, res, next) => {
   try {
+    const studyId: number = Number(req.params.id);
+
+    const study = await prisma.study.findUnique({
+      where: {
+        id: studyId,
+      },
+    });
+
+    if (!study) {
+      // 미들웨어 사용을 위해 error 객체를 throw
+      const error: CustomError = new Error(
+        "삭제할 스터디가 존재하지 않습니다!"
+      ); // error 클래스를 정의해두는 것도 좋을 듯!
+      error.status = 404;
+      throw error;
+    }
+
+    const now = new Date();
+
+    await prisma.$transaction([
+      await prisma.study.update({
+        where: {
+          id: studyId,
+        },
+        data: {
+          deletedAt: now,
+        },
+      }),
+      await prisma.studyDeleteLog.create({
+        data: {
+          studyId,
+          deletedAt: now,
+        },
+      }),
+    ]);
+
+    res.status(200).send("스터디가 삭제되었습니다!");
   } catch (error) {
     next(error);
   }
